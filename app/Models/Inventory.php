f5b25f57afea865795fa1e7d2f462b24d187a90b
Rho <78846865+loftyyyy@users.noreply.github.com>
2025-12-08 23:39:16 +0800

    Added fillables and relationship on Model/Inventory

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -12,6 +12,22 @@ class Inventory extends Model
     protected $table = 'inventories';
     protected $primaryKey = 'item_id';
     protected $fillable = [
-
+        'item_type',
+        'name',
+        'size',
+        'color',
+        'design',
+        'condition',
+        'rental_price',
+        'quantity',
+        'status_id'
     ];
+
+    protected $casts = [
+        'rental_price' => 'decimal:2',
+        'quantity' => 'integer',
+    ];
+    public function status(){
+        return $this->belongsTo(InventoryStatus::class, 'status_id', 'status_id');
+    }
 }
