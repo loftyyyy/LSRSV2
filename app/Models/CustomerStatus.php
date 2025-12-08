4337342efe76defccc82997db3739041e887d436
Rho <78846865+loftyyyy@users.noreply.github.com>
2025-12-08 23:29:46 +0800

    Added fillables for Models/CustomerStatus

--- app/Models/CustomerStatus.php
+++ app/Models/CustomerStatus.php
@@ -9,4 +9,12 @@ class CustomerStatus extends Model
 {
     /** @use HasFactory<\Database\Factories\CustomerStatusFactory> */
     use HasFactory;
+
+    protected $table = 'customer_status';
+    protected $primaryKey = 'status_id';
+    protected $fillable = [
+        'status_id',
+        'status_name',
+        'reason'
+    ];
 }
