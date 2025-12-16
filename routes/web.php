44d9f04676cc84261ca9bb3155d5f18993054f17
Rho <78846865+loftyyyy@users.noreply.github.com>
2025-12-17 03:09:28 +0800

    Implementing the web.php

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', function () {
