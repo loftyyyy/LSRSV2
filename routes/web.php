24e0c2a3c185f390bb96ea87eaa7e5e3603fc81f
Rho <78846865+loftyyyy@users.noreply.github.com>
2025-12-19 01:45:38 +0800

    Implemented the CRUD api on web.php

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,47 @@ Route::middleware('auth')->group(function () {
     Route::get('/invoices', [InvoiceController::class, 'showInvoicePage'])->name('invoices');
     Route::get('/payments', [PaymentController::class, 'showPaymentPage'])->name('payments');
 });
+
+Route::middleware('auth')->prefix('api')->group(function (){
+    // Customer CRUD
+    Route::get('/customers', [CustomerController::class, 'index']);
+    Route::post('/customers', [CustomerController::class, 'store']);
+    Route::get('/customers/{id}', [CustomerController::class, 'show']);
+    Route::put('/customers/{id}', [CustomerController::class, 'update']);
+    Route::delete('/customers/{id}', [CustomerController::class, 'destroy']);
+
+    // Inventory CRUD
+    Route::get('/inventories', [InventoryController::class, 'index']);
+    Route::post('/inventories', [InventoryController::class, 'store']);
+    Route::get('/inventories/{id}', [InventoryController::class, 'show']);
+    Route::put('/inventories/{id}', [InventoryController::class, 'update']);
+    Route::delete('/inventories/{id}', [InventoryController::class, 'destroy']);
+
+    // Invoice CRUD
+    Route::get('/invoices', [InvoiceController::class, 'index']);
+    Route::post('/invoices', [InvoiceController::class, 'store']);
+    Route::get('/invoices/{id}', [InvoiceController::class, 'show']);
+    Route::put('/invoices/{id}', [InvoiceController::class, 'update']);
+    Route::delete('/invoices/{id}', [InvoiceController::class, 'destroy']);
+
+    // Payment CRUD
+    Route::get('/payments', [PaymentController::class, 'index']);
+    Route::post('/payments', [PaymentController::class, 'store']);
+    Route::get('/payments/{id}', [PaymentController::class, 'show']);
+    Route::put('/payments/{id}', [PaymentController::class, 'update']);
+    Route::delete('/payments/{id}', [PaymentController::class, 'destroy']);
+
+    // Rental CRUD
+    Route::get('/rentals', [RentalController::class, 'index']);
+    Route::post('/rentals', [RentalController::class, 'store']);
+    Route::get('/rentals/{id}', [RentalController::class, 'show']);
+    Route::put('/rentals/{id}', [RentalController::class, 'update']);
+    Route::delete('/rentals/{id}', [RentalController::class, 'destroy']);
+
+    // Reservation CRUD
+    Route::get('/reservations', [ReservationController::class, 'index']);
+    Route::post('/reservations', [ReservationController::class, 'store']);
+    Route::get('/reservations/{id}', [ReservationController::class, 'show']);
+    Route::put('/reservations/{id}', [ReservationController::class, 'update']);
+    Route::delete('/reservations/{id}', [ReservationController::class, 'destroy']);
+});
