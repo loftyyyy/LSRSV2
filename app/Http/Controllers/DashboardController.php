73c4e66e9931c9600e631994a8d7ab5f7c9cc3cd
Rho <78846865+loftyyyy@users.noreply.github.com>
2025-12-19 00:29:42 +0800

    added dashboard page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,4 +5,11 @@ use Illuminate\Routing\Controller;
 class DashboardController extends Controller
 {
 
+    /**
+     * Display Dashboard Page
+     */
+    public function showDashboardPage()
+    {
+        return view('dashboard');
+    }
 }
