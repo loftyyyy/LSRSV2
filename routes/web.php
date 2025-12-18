ce1e71478d32841b6a48e3a424c06714553a2751
Rho <78846865+loftyyyy@users.noreply.github.com>
2025-12-19 01:26:42 +0800

    Implemented the routes for showPages

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\InventoryController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\RentalController;
+use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/landing', function () {
     return view('welcome');
 });
 
@@ -15,8 +22,11 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    });
-
+    Route::get('/dashboard', [DashboardController::class, 'showDashboardPage'])->name('dashboard');
+    Route::get('/customers', [CustomerController::class, 'showCustomerPage'])->name('customers');
+    Route::get('/inventories', [InventoryController::class, 'showInventoryPage'])->name('inventories');
+    Route::get('/reservations', [ReservationController::class, 'showReservationPage'])->name('reservations');
+    Route::get('/rentals', [RentalController::class, 'showRentalPage'])->name('rentals');
+    Route::get('/invoices', [InvoiceController::class, 'showInvoicePage'])->name('invoices');
+    Route::get('/payments', [PaymentController::class, 'showPaymentPage'])->name('payments');
 });
