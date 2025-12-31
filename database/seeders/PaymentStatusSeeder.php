a049c4a72a564bdba6e623c2bf98e538c2f7e80a
Rho <78846865+loftyyyy@users.noreply.github.com>
2025-12-31 23:19:22 +0800

    Added seeder for PaymentStatus

--- database/seeders/PaymentStatusSeeder.php
+++ database/seeders/PaymentStatusSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PaymentStatusSeeder extends Seeder
 {
@@ -12,6 +13,14 @@ class PaymentStatusSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('payment_statuses')->insert([
+            ['status_name' => 'pending'],
+            ['status_name' => 'paid'],
+            ['status_name' => 'refunded'],
+            ['status_name' => 'cancelled'],
+            ['status_name' => 'voided'],
+            ['status_name' => 'failed'],
+
+        ]);
     }
 }
