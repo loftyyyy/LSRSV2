e4f76ffbfbb2c0edaba412deb6f3723e33d4e48a
Rho <78846865+loftyyyy@users.noreply.github.com>
2025-12-31 23:26:29 +0800

    Added seeder for RentalStatus

--- database/seeders/RentalStatusSeeder.php
+++ database/seeders/RentalStatusSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RentalStatusSeeder extends Seeder
 {
@@ -12,6 +13,10 @@ class RentalStatusSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('rental_statuses')->insert([
+            ['status_name' => 'rented'],
+            ['status_name' => 'returned'],
+            ['status_name' => 'overdue'],
+        ]);
     }
 }
