5a6a2618b7fbae3d0a2cca01cf3aac4ebfe6898c
Rho <78846865+loftyyyy@users.noreply.github.com>
2025-12-31 23:13:29 +0800

    Added seeder for InventoryStatus

--- database/seeders/InventoryStatusSeeder.php
+++ database/seeders/InventoryStatusSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Inventory;
+use App\Models\InventoryStatus;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class InventoryStatusSeeder extends Seeder
 {
@@ -12,6 +16,11 @@ class InventoryStatusSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('inventory_statuses')->insert([
+            ['status_name' => 'available'],
+            ['status_name' => 'rented'],
+            ['status_name' => 'maintenance'],
+            ['status_name' => 'retired'],
+        ]);
     }
 }
