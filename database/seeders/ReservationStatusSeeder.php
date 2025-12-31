b42c8c1a9aabaeed4f527f81ab7f1aa8b376a430
Rho <78846865+loftyyyy@users.noreply.github.com>
2025-12-31 23:29:08 +0800

    Added seeder for ReservationStatus

--- database/seeders/ReservationStatusSeeder.php
+++ database/seeders/ReservationStatusSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ReservationStatusSeeder extends Seeder
 {
@@ -12,6 +13,11 @@ class ReservationStatusSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('reservation_status')->insert([
+            ['status_name' => 'confirmed'],
+            ['status_name' => 'cancelled'],
+            ['status_name' => 'expired'],
+
+        ]);
     }
 }
