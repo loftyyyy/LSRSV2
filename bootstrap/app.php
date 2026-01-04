3186be9e0f7c32eb2f0bfd28043eed5293787a03
Rho <78846865+loftyyyy@users.noreply.github.com>
2026-01-04 23:31:33 +0800

    CSRF bug fixed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,12 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
        health: '/up',
    )
     ->withMiddleware(function (Middleware $middleware): void {
-//        $middleware->validateCsrfTokens(except: [
-//            '/register',
-//            '/login',
-//            '/logout',
-//            'api/*',
-//        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
