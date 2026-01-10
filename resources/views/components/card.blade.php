7893141343f94525489da272e6a00ed3e1bf0a67
Rho <78846865+loftyyyy@users.noreply.github.com>
2026-01-10 10:47:11 +0800

    Implementing the rental page from the design

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -4,11 +4,7 @@
 
 <div
     {{ $attributes->merge([
-        'class' => "rounded-2xl border border-neutral-900
-            bg-neutral-950/60
-            shadow-[0_18px_60px_rgba(0,0,0,0.65)]
-            backdrop-blur-sm
-            {$padding}
+        'class' => "rounded-2xl border dark:border-neutral-900 border-neutral-100 dark:bg-neutral-950/60 bg-neutral-50/60 shadow-[0_18px_60px_rgba(0,0,0,0.65)] backdrop-blur-sm {$padding}
         "
     ]) }}
 >
