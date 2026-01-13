3da103f300bedefd1556dddf143cf2125a038cbb
Rho <78846865+loftyyyy@users.noreply.github.com>
2026-01-14 00:03:18 +0800

    Implemented the inventories page

--- resources/views/inventories/index.blade.php
+++ resources/views/inventories/index.blade.php
@@ -40,7 +40,7 @@
 
                 <button class="inline-flex items-center gap-2 rounded-xl px-4 py-2 text-[14px] font-medium bg-violet-600 text-white dark:hover:text-white hover:text-black dark:text-black hover:bg-violet-500 shadow-violet-600/40 transition-colors duration-300 ease-in-out">
                     <x-icon name="plus" class="h-4 w-4" />
-                    <span>Add Customer</span>
+                    <span>Add Item</span>
                 </button>
             </div>
         </div>
