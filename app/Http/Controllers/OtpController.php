e69e31485b1b29b7fc126a9c66fa16a359b73e95
Rho <78846865+loftyyyy@users.noreply.github.com>
2026-01-18 00:48:38 +0800

    Implemented the OtpController

--- app/Http/Controllers/OtpController.php
+++ app/Http/Controllers/OtpController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\OTPController;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Services\OtpService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -11,11 +12,28 @@ class OtpController extends Controller
 {
     public function generateOtp(Request $request): JsonResponse
     {
-        $credential  = $request->validate([
-            'email' => ['required', 'email'],
-        ]);
+        try{
+            $request->validate([
+                'email' => ['required', 'email'],
+            ]);
+
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user) {
+                return resonse()->json([
+                    'email' => 'The provided credentials do not match our records.',
+                ]);
+            }
 
 
+
+            return response()->json([
+                'message' => 'OTP generated successfully',
+            ]);
+
+        }catch (\Exception $exception){
+            return response()->json(['message' => $exception->getMessage()]);
+        }
     }
 
 
