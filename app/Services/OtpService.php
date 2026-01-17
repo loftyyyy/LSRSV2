e3122f7a8b99ddfb8d8669352f851083cb1d72cd
Rho <78846865+loftyyyy@users.noreply.github.com>
2026-01-18 00:31:07 +0800

    Implemented the OtpService

--- app/Services/OtpService.php
+++ app/Services/OtpService.php
@@ -1,14 +1,32 @@
 <?php
 namespace App\Services;
+use Illuminate\Support\Facades\Redis;
+
 class OtpService {
 
 
-    public function generateOtp(String $email)
+    public function generateOtp(String $email): String
     {
         $key = "otp:" . $email;
         $otp = sprintf("%06d", random_int(0, 999999));
-        
 
+        Redis::setex($key, 300, $otp);
 
+        return $key;
     }
+
+    public function verifyOtp(String $email, String $otp): Boolean
+    {
+        $key = "otp:" . $email;
+
+        $value = Redis::get($key);
+        return $value === $otp;
+    }
+
+    public function deleteOtp(String $email): void
+    {
+        $key = "otp:" . $email;
+        Redis::del($key);
+    }
+
 }
