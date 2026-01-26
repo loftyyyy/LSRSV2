aae486c3888f396c8518951953252c0e4cd7f349
Rho <78846865+loftyyyy@users.noreply.github.com>
2026-01-27 03:03:40 +0800

    Added CSRF token for the adding of customers

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,8 +3,11 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-// Set CSRF token from meta tag
-const token = document.querySelector('meta[name="csrf-token"]');
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.getAttribute('content');
-}
+// Set CSRF token from meta tag on every request
+window.axios.interceptors.request.use(config => {
+    const token = document.querySelector('meta[name="csrf-token"]');
+    if (token) {
+        config.headers['X-CSRF-TOKEN'] = token.getAttribute('content');
+    }
+    return config;
+});
