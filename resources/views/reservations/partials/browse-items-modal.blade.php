9a7bd8ad70509f96909597d010852a3ee8f05165
Rho <78846865+loftyyyy@users.noreply.github.com>
2026-02-08 23:59:10 +0800

    style: remove close button border in browse modal

--- resources/views/reservations/partials/browse-items-modal.blade.php
+++ resources/views/reservations/partials/browse-items-modal.blade.php
@@ -17,7 +17,7 @@
                 <button
                     type="button"
                     onclick="closeBrowseItemsModal()"
-                    class="inline-flex h-8 w-8 items-center justify-center rounded-lg border border-neutral-200 text-neutral-500 hover:text-neutral-800 hover:bg-white dark:border-neutral-700 dark:text-neutral-300 dark:hover:text-neutral-100 dark:hover:bg-neutral-800 transition-colors duration-200"
+                    class="inline-flex h-8 w-8 items-center justify-center rounded-lg text-neutral-500 hover:text-neutral-800 hover:bg-white dark:text-neutral-300 dark:hover:text-neutral-100 dark:hover:bg-neutral-800 transition-colors duration-200"
                 >
                     &times;
                 </button>
